e7a6abcc522830ecdb088825b0dfea97bbda10c6
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2022-12-26 00:50:51 -0300

    first commit

---
+++ app/validacao.js
@@ -0,0 +1,56 @@
+function verificaSeUmChutePossuiUmValorValido(chute) {
+    const numero = +chute;
+
+    if (chuteForInvalido(numero)) {
+        elementoChute.innerHTML += `<div>Valor inválido, o chute deve ser um número.</div>`
+        return
+    }
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+        elementoChute.innerHTML += `<div>Valor inválido, fale um número entre ${menorValor} e ${maiorValor}.</div>`
+        return
+    }
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}.</h3>
+
+            <div class="btn">
+                <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width: 1.2em;">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+                </svg>
+            </div>
+            
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+            <div class="resultado">O número secreto é menor 
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width: 1.2em;">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3" />
+                </svg>
+            </div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+            <div class="resultado">O número secreto é maior 
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width: 1.2em;">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75L12 3m0 0l3.75 3.75M12 3v18" />
+                </svg>
+            </div>
+        `
+    }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor;
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload();
+    }
+});
