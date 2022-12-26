f1e0816e1d131e5e5a4f649cf5b437915fb31f5e
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2022-12-26 01:31:01 -0300

    add tela de game over

--- app/validacao.js
+++ app/validacao.js
@@ -1,6 +1,17 @@
 function verificaSeUmChutePossuiUmValorValido(chute) {
     const numero = +chute;
 
+    if (chute === 'game over') {
+        document.body.innerHTML = `
+            <h2>GAME OVER</h2>
+            <h3>Precione o botão para jogar novamente.</h3>
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" style="width: 1.2em; margin-left:0.5em;">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+                </svg>
+            </button>
+        `
+    }
     if (chuteForInvalido(numero)) {
         elementoChute.innerHTML += `<div>Valor inválido, o chute deve ser um número.</div>`
         return
